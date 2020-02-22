8f725015516d5a86919387e4fb515e44f302a7c2
Jean-Sebastien BEVILACQUA <realitix@gmail.com>
2020-02-22 10:38:28 +0100

    Remove useless init

--- app/src/main/java/com/realitix/mealassistant/viewmodel/ReceipeStepViewModel.kt
+++ app/src/main/java/com/realitix/mealassistant/viewmodel/ReceipeStepViewModel.kt
@@ -13,14 +13,5 @@ import kotlinx.coroutines.runBlocking
 
 class ReceipeStepViewModel constructor(var repository: ReceipeRepository, receipeId: Long, stepId: Long) : ViewModel() {
     val receipe: LiveData<Receipe> = repository.getReceipe(receipeId)
-    val step: LiveData<ReceipeStepDao.ReceipeStepFull> = {
-        var rid = stepId
-        runBlocking {
-            if(!repository.hasReceipeStep(rid)) {
-                val r = ReceipeStep(receipeId, 0, "Nouvelle étape", 0)
-                rid = repository.createReceipeStep(r)
-            }
-        }
-        repository.getReceipeStepFull(rid)
-    }()
+    val step: LiveData<ReceipeStepDao.ReceipeStepFull> = repository.getReceipeStepFull(stepId)
 }
